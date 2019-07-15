f865604b75a82789d10cb4a1de905412eef24cf8
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2019-07-15 01:20:43 -0300

    commit inicial do frontend e backend

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');  // OK
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+// Permitir acesso via http
+const server = require('http').Server(app);
+// Permitir acesso via websocket
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb+srv://admin:47f7FvUGkJWkf6yg@cluster0-3usga.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+});
+
+//Midleware para qe todos possam usar o io
+app.use((req, res, next) => {
+    req.io = io; // Websocket
+    next(); //Garante que todos continuem sendo executados
+});
+
+//Qualquer aplicação tem acesso ao backend
+app.use(cors());
+
+//Ao acessar a rota files, redireciona para as imagens redimensionadas
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+server.listen(3333);
